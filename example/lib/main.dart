9cb99d082caad6bb856400fdd3d4b79b6429e0bf
Pierre Moati <pmoati@kaliop.com>
2022-12-23 02:43:58 +0100

    added class with generic

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,17 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:cool_dropdown/cool_dropdown.dart';
+import 'package:cool_dropdown/drop_down_params.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+class FruitQuantity {
+  final String name;
+  final int quantity;
+
+  FruitQuantity({this.name, this.quantity});
+}
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
-List dropdownItemList = [];
 
 List<String> pokemons = [
   'pikachu',
@@ -23,60 +29,60 @@ List<String> pokemons = [
   'psyduck',
   'meowth'
 ];
-List<String> fruits = [
-  'apple',
-  'banana',
-  'grapes',
-  'lemon',
-  'melon',
-  'orange',
-  'pineapple',
-  'strawberry',
-  'watermelon',
+
+List<DropDownParams<FruitQuantity>> dropdownItemList = [];
+
+List<FruitQuantity> fruits = [
+  FruitQuantity(name: 'apple', quantity: 12),
+  FruitQuantity(name: 'banana', quantity: 4),
+  FruitQuantity(name: 'grapes', quantity: 2),
+  FruitQuantity(name: 'lemon', quantity: 8),
+  FruitQuantity(name: 'melon', quantity: 2),
+  FruitQuantity(name: 'orange', quantity: 7),
+  FruitQuantity(name: 'pineapple', quantity: 1),
+  FruitQuantity(name: 'strawberry', quantity: 4),
+  FruitQuantity(name: 'watermelon', quantity: 2),
 ];
 
 class _MyAppState extends State<MyApp> {
-  List<Map> pokemonsMap = [];
+  List<DropDownParams<String>> pokemonsDropdownList = [];
   @override
   void initState() {
-    for (var i = 0; i < pokemons.length; i++) {
-      pokemonsMap.add({
-        'label': '${pokemons[i]}',
-        'value': '${pokemons[i]}',
-        'icon': Container(
-          height: 25,
-          width: 25,
+    pokemonsDropdownList = pokemons.map((pokemon) => DropDownParams(
+          label: pokemon,
+          value: pokemon,
+          icon: Container(
+            height: 25,
+            width: 25,
+            child: SvgPicture.asset(
+              'assets/$pokemon.svg',
+            ),
+          ),
+        ));
+
+    for (var i = 0; i < fruits.length; i++) {
+      dropdownItemList.add(DropDownParams<FruitQuantity>(
+        label: fruits[i].name == 'melon' ? 'melon sugar high' : fruits[i].name,
+        // 'label': '${fruits[i]}',
+        value: fruits[i],
+        icon: Container(
+          key: UniqueKey(),
+          height: 20,
+          width: 20,
           child: SvgPicture.asset(
-            'assets/${pokemons[i]}.svg',
+            'assets/${fruits[i]}.svg',
           ),
         ),
-      });
-    }
-    for (var i = 0; i < fruits.length; i++) {
-      dropdownItemList.add(
-        {
-          'label': fruits[i] == 'melon' ? 'melon sugar high' : '${fruits[i]}',
-          // 'label': '${fruits[i]}',
-          'value': '${fruits[i]}',
-          'icon': Container(
-            key: UniqueKey(),
-            height: 20,
-            width: 20,
-            child: SvgPicture.asset(
-              'assets/${fruits[i]}.svg',
-            ),
+        selectedIcon: Container(
+          key: UniqueKey(),
+          width: 20,
+          height: 20,
+          child: SvgPicture.asset(
+            'assets/${fruits[i]}.svg',
+            color: Color(0xFF6FCC76),
           ),
-          'selectedIcon': Container(
-            key: UniqueKey(),
-            width: 20,
-            height: 20,
-            child: SvgPicture.asset(
-              'assets/${fruits[i]}.svg',
-              color: Color(0xFF6FCC76),
-            ),
-          ),
-        },
-      );
+        ),
+      ));
     }
     super.initState();
   }
@@ -96,7 +102,7 @@ class _MyAppState extends State<MyApp> {
               height: 100,
             ),
             Center(
-              child: CoolDropdown(
+              child: CoolDropdown<FruitQuantity>(
                 dropdownList: dropdownItemList,
                 onChange: (selectedItem) {
                   print(selectedItem);
@@ -147,8 +153,8 @@ class _MyAppState extends State<MyApp> {
               height: 200,
             ),
             Center(
-              child: CoolDropdown(
-                dropdownList: pokemonsMap,
+              child: CoolDropdown<String>(
+                dropdownList: pokemonsDropdownList,
                 dropdownItemPadding: EdgeInsets.zero,
                 onChange: (dropdownItem) {},
                 resultHeight: 50,
